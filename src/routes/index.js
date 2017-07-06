3b850511df407a35f803118ca14c78b7e240cb8f
Logan Rakai <lmrakai@gmail.com>
2017-07-06 13:56:41 -0600

    Select first network interface IP for localAddress display

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ Object.keys(ifaces).forEach(function (ifname) {
   var alias = 0;
 
   ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
+    if ('IPv4' !== iface.family || iface.internal !== false || localAddress !== '') {
       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
       return;
     }
